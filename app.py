98a5a76bdb2c49322d72e503f294882296b239f7
Adam <adamlevitz@gmail.com>
2020-01-23 17:01:01 -0800

    Complete step 6, starting on 7

--- app.py
+++ app.py
@@ -1,52 +1,75 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys
+from surveys import satisfaction_survey as survey
 
-responses = []
+RESPONSES_KEY = 'response'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 @app.route("/")
 def questions_page():
-    return render_template("/base.html")
-    
+    return render_template("/start_survey.html", survey=survey)
+
+@app.route("/start_survey", methods=["POST"])
 def start_survey():
+    session[RESPONSES_KEY] = []
     return redirect("/questions/0")
 
-@app.route("/questions/0", methods=["POST"])
-def ask_question_0():
-    question_0 = surveys["satisfaction"].questions[0].question
-    choice_0 = surveys["satisfaction"].questions[0].choice[0]
-    choice_1 = surveys["satisfaction"].questions[0].choice[1]
 
-    return render_template("/question_template", question=question_0, option_0=choice_0, option_1=choice_1)
-    responses.append(request.form["choice"])
+@app.route("/questions/<int:qid>")
+def ask_question(qid):
+    if qid != len(session[RESPONSES_KEY]):
+        return redirect(f"/questions/{len(session[RESPONSES_KEY])}") 
+    if len(session[RESPONSES_KEY]) == len(survey.questions):
+        return redirect("/complete")
+    question = survey.questions[qid].question
+    choice_0 = survey.questions[qid].choices[0]
+    choice_1 = survey.questions[qid].choices[1]
+    return render_template("/question_template.html", question=question,
+    option_0=choice_0, option_1=choice_1)
 
-    return redirect("/questions/1")
+@app.route("/complete")
+def completed_survey():
+    return render_template('complete.html')
 
-@app.route("/questions/1", methods=["POST"])
-def ask_question_1():
-    question_1 = surveys["satisfaction"].questions[1].question
-    choice_0 = surveys["satisfaction"].questions[1].choice[0]
-    choice_1 = surveys["satisfaction"].questions[1].choice[1]
+# @app.route("/questions/<int:qid>", methods=["POST"])
+# def ask_question():
+#     question_1 = survey.questions[qid].question
+#     choice_0 = survey.questions[qid].choice[0]
+#     choice_1 = survey.questions[qid].choice[1]
+#     return redirect("/questions/2", question=question_qid, option_0=choice_0, option_1=choice_1)
+
+
+# @app.route("/questions/2", methods=["POST"])
+# def ask_question_2():
+#     question_2 = survey.questions[2].question
+#     choice_0 = survey.questions[2].choice[0]
+#     choice_1 = survey.questions[2].choice[1]
+#     return redirect("/questions/3", question=question_2, option_0=choice_0, option_1=choice_1)
+
+
+# @app.route("/questions/3", methods=["POST"])
+# def ask_question_3():
+#     question_3 = survey.questions[3].question
+#     choice_0 = survey.questions[3].choice[0]
+#     choice_1 = survey.questions[3].choice[1]
+#     return redirect("/questions/3", question=question_3, option_0=choice_0, option_1=choice_1)
+#     return redirect("/thanks")
+
+@app.route("/answer", methods=["POST"])
+def answer_handler():
+    answer = request.form['choice']
     
-    return redirect("/questions/2", question=question_1, option_0=choice_0, option_1=choice_1)
+    responses = session[RESPONSES_KEY]
+    responses.append(answer)
+    session[RESPONSES_KEY] = responses
 
-@app.route("/questions/2", methods=["POST"])
-def ask_question_2():
-    question_2 = surveys["satisfaction"].questions[2].question
-    choice_0 = surveys["satisfaction"].questions[2].choice[0]
-    choice_1 = surveys["satisfaction"].questions[2].choice[1]
-    
-    return redirect("/questions/3", question=question_2, option_0=choice_0, option_1=choice_1)
+    print('responses =', responses)
 
+    if (len(responses) == len(survey.questions)):
+        return redirect('/complete')
 
-@app.route("/questions/3", methods=["POST"])
-def ask_question_3():
-    question_3 = surveys["satisfaction"].questions[3].question
-    choice_0 = surveys["satisfaction"].questions[3].choice[0]
-    choice_1 = surveys["satisfaction"].questions[3].choice[1]
-    
-    return redirect("/questions/3", question=question_3, option_0=choice_0, option_1=choice_1)
-    return redirect("/thanks")
+    else:
+        return redirect(f"/questions/{len(responses)}")
